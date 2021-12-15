40bb7e06b39dd8e068d5b6a079dd274fea5eb34d
furia03 <furia03@users.noreply.github.com>
2021-12-15 09:27:12 +0000

    deploy: 8976bbd1c7e4b7a91a4edfda7c46f5c556e7f553

---
+++ precache-manifest.8f7705a549ed64a5f5b917368fcbf53a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e51db479e12f46974b",
+    "url": "/nine/css/app.9ec0eb38.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/nine/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "251283b0b793c92d0b4bd5d78690eca2",
+    "url": "/nine/index.html"
+  },
+  {
+    "revision": "2b5bb577fc9ac3c228e3",
+    "url": "/nine/js/about.2126a30a.js"
+  },
+  {
+    "revision": "84e51db479e12f46974b",
+    "url": "/nine/js/app.5786e9f0.js"
+  },
+  {
+    "revision": "419bacddb4c343873286",
+    "url": "/nine/js/chunk-vendors.34879c67.js"
+  },
+  {
+    "revision": "905da656e9760c216f372f20d576ed89",
+    "url": "/nine/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nine/robots.txt"
+  }
+]);
