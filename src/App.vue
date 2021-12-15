0c1bf7959a18053895a76109ca37e0d6065b03bc
furia0928 <furia0928@gmail.com>
2021-12-15 17:55:56 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home111</router-link> |
+      <router-link to="/">Home12211</router-link> |
       <router-link to="/about">About222</router-link>
     </div>
     <router-view />
