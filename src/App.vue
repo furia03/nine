7a3bb502b709cae0784ce61071c12be81316ba17
furia0928 <furia0928@gmail.com>
2021-12-16 11:36:29 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">555ㅇㄹㅇㄹ</router-link> |
-      <router-link to="/about">About222</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
