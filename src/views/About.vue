930b94bcf6963aee261904c37bb1320b003a50a0
furia0928 <furia0928@gmail.com>
2022-01-19 14:13:33 +0900

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,56 @@
     <h1>This is an about page {{ $route.query }}</h1>
   </div>
 </template>
+<script>
+export default {
+  name: "App",
+  metaInfo() {
+    return {
+      title: "furia03",
+      titleTemplate: "about | %s",
+      meta: [
+        {
+          name: "title",
+          content: "about | furia03",
+          vmid: "title",
+        },
+        {
+          name: "description",
+          content: "",
+          vmid: "description",
+        },
+        {
+          name: "keywords",
+          content: "",
+          vmid: "keywords",
+        },
+        {
+          property: "og:type",
+          content: "website",
+          vmid: "og:type",
+        },
+        {
+          property: "og:title",
+          content: "furia03",
+          vmid: "og:title",
+        },
+        {
+          property: "og:description",
+          content: "",
+          vmid: "og:description",
+        },
+        {
+          property: "og:image",
+          content: process.env.BASE_URL + "images/share.jpg",
+          vmid: "og:image",
+        },
+        {
+          property: "og:url",
+          content: process.env.VUE_APP_URL,
+          vmid: "og:url",
+        },
+      ],
+    };
+  },
+};
+</script>
