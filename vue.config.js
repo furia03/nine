513af3aafc846247aa4959896806f10db53f3c30
furia0928 <furia0928@gmail.com>
2022-01-19 14:19:51 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/nine/" : "/",
   outputDir: "docs",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "PC Page",
+    },
+    about: "src/main.js",
+  },
 };
